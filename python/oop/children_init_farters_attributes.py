e829ed49e053d3308847a261654f7b6f10f0b7e7
liuzz <liuzhizhi123@126.com>
2014-04-17 16:58:44 +0800

    字典 命令行解析等

---
+++ python/oop/children_init_farters_attributes.py
@@ -0,0 +1,27 @@
+﻿#-*- coding: utf-8 -*-
+#children_init_farters_attributes.py   python2.7.x
+'''
+子类初始化父类的属性, 父类中使用属性
+'''
+
+class Farter(object):
+    farter_attr = 288
+
+    def print_attr(self):
+        print  self.farter_attr+2000
+
+
+class Child1(Farter):
+
+    def __init__(self, farter_attr):
+        self.farter_attr = farter_attr
+
+    def get_farter_attr(self):
+        #不初始化父类，直接打印
+        self.print_attr()
+        print self.farter_attr
+
+
+if __name__=='__main__':
+    #子类属性会覆盖父类原来的属性，调用父类方法的时候会使用子类新的属性
+    Child1(100).get_farter_attr()
---
+++ python/parse_commond_lines.py
@@ -0,0 +1,26 @@
+﻿#--*-- coding:utf-8 --*--
+#parse_commond_lines.py   python2.7x
+'''
+解析命令行  参数  显示help信息
+作为一个命令行工具的模板
+常用的
+--help  帮助
+-v  版本
+-n  名字
+-list  数组
+'''
+import argparse
+VERSION = 1.0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--name",help ="input name of something", type=str)
+parser.add_argument("-m", "--mulname",help ="input names of something", type=str,nargs = '*')
+parser.add_argument("-v","--version", help ="the version of this tools", action="store_true")
+args = parser.parse_args()
+
+if args.version:
+    print VERSION
+elif args.name:
+    print "name is %s"%args.name
+elif args.mulname:
+    print args.mulname
