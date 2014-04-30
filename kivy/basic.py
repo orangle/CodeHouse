901dc4bbeae6888bb23d49037d27dae60a268b4f
liuzz <liuzhizhi123@126.com>
2014-04-30 16:14:35 +0800

    [orangleliu]kivy pratices

---
+++ kivy/basic.py
@@ -0,0 +1,34 @@
+﻿# -*- coding: utf-8 -*-
+#basic.py   python2.7.x
+#orangleliu@gmail.com    2014-04-30
+'''
+kivy 基本使用方法
+'''
+
+import kivy
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+class LoginScreen(GridLayout):
+
+    def  __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols =2
+        self.add_widget(Label(text=u"Username: "))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+
+        self.add_widget(Label(text=u'Password:'))
+        self.password = TextInput(password=True, multilne=False)
+        self.add_widget(self.password)
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+if __name__ == '__main__':
+    MyApp().run()
---
+++ kivy/pong_game.py
@@ -0,0 +1,23 @@
+﻿# -*- coding: utf-8 -*-
+#pong_game.py   python2.7.x
+#orangleliu@gmail.com    2014-04-30
+'''
+编写一个乒乓球的小应用
+'''
+
+from kivy.app  import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
+
+if __name__ == '__main__':
+    PongApp().run()
+
+
