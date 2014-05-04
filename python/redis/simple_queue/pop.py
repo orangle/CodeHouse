36240714d69df56baf2a0e20b849b26d519116b8
liuzz <liuzhizhi123@126.com>
2014-05-04 16:18:16 +0800

    [orangleliu]redis 学习，消息队列简单模拟

---
+++ python/redis/simple_queue/pop.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+#pop.py   python2.7.x
+#orangleliu@gmail.com    2014-05-04
+'''
+模拟任务队列从redis中取出
+python pop.py
+'''
+
+import sys
+from redis import Redis
+
+redis = Redis(host="localhost", port=6379)
+while 1:
+    res = redis.rpop('time_queue')
+    if res== None:
+        pass
+    else:
+        print str(res)
+
+
---
+++ python/redis/simple_queue/push.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+#push.py   python2.7.x
+#orangleliu@gmail.com    2014-05-04
+'''
+模拟任务队列写入到redis中
+使用方式
+python  push.py  1
+'''
+import time
+from redis import Redis
+import sys
+
+redis = Redis(host='localhost', port=6379)
+while 1:
+    now = time.strftime("%Y-%m-%d %H:%M:%S") + ' | ' +sys.argv[1]
+    print now
+    redis.lpush('time_queue', now)
+    time.sleep(2)
