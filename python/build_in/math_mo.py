353d2c8824364851fa64fa10b8f9fdc8c6335b9f
liuzz <liuzhizhi123@126.com>
2014-05-24 18:25:05 +0800

    [orangleliu]

---
+++ python/build_in/math_mo.py
@@ -0,0 +1,57 @@
+﻿#-*- coding: utf-8 -*-
+#python2.7x
+#author: orangleliu  @2014-05-24
+'''
+math_mo.py
+math 几个常见的用法
+'acos',
+'acosh'
+'asin',
+'asinh'
+'atan',
+'atan2'
+'atanh'
+'ceil',     比当前数字x大或者相等的最小整数
+'copysi
+'cos',
+'cosh',
+'degree
+'e',
+'erf',
+'erfc',
+'exp',
+'expm1'
+'fabs',      获取绝对值
+'factorial’   求从1到某个数的阶乘，输入参数为整数
+'floor'   比当前x小或者相等的最大整数，相当于取整
+'fmod',   和x%y的作用是一样的，这个方法更适合使用浮点数情况，%更适合使用在整数情况
+'frexp'
+'fsum',   用来对一个整形的可迭代数据，进行求和，能避免精度缺失的问题
+'gamma'
+'hypot'
+'isinf'   是正无穷还是负无穷
+'isnan'  是否为Nan
+'ldexp'
+'lgamma
+'log',
+'log10'
+'log1p'
+'modf',
+'pi',    圆周率
+'pow',
+'radian
+'sin',
+'sinh',
+'sqrt',
+'tan',
+'tanh',
+'trunc'
+'''
+
+#浮点型数字，向正无穷趋近时第一个整数
+'''
+In [37]: math.ceil(2.111)
+Out[37]: 3.0
+'''
+
+
---
+++ python/build_in/random_mo.py
@@ -0,0 +1,33 @@
+﻿#-*- coding: utf-8 -*-
+#python2.7x
+#author: orangleliu  @2014-05-24
+'''
+random使用方法
+'''
+
+import  random
+
+#0-1之间的浮点数
+print random.random()
+
+#某两个数之间的浮点数
+print random.uniform(1, 20)
+
+#比较常用的： 求某个连续区间的整数随机数
+print random.randint(1, 100)
+
+#去某个步长一定的序列的整数随机数
+#0 2 4...100 之间的随机整数
+print random.randrange(0, 101, 2)
+
+
+#一串字符串中任意随机元素（例如一个生成验证码）
+print random.choice('1234567890abcdefghijklmnopqrstuvwsyz')
+
+#从序列中任意选取多个，更适合做 几位的验证码
+print random.sample([1,2,3,4,5,6,7,8,9,0],4)
+
+#随机排序
+items = [1, 2, 3, 4, 5, 6, 7]
+random.shuffle(items)
+print items
