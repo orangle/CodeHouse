a4f264e00b1f6f1d285f2d36005e7074f87487a4
liuzz <liuzhizhi123@126.com>
2014-07-02 13:57:22 +0800

    [orangleliu]interview

---
+++ python/common_mistakes_interview/Q_small.py
@@ -0,0 +1,20 @@
+﻿#-*- coding: utf-8 -*-
+#python2.7x
+#author: orangleliu  @2014-06-25
+'''
+一些比较简短的问题和回答
+'''
+
+#say('Hello')('World')
+def say(a):
+    def hello(b):
+        return a+" "+b
+    return hello
+
+print  say('Hello')('World')
+
+
+say1 = lambda x: lambda y: x + " " + y
+print say1('Hello')('World')
+
+print '*'*50
