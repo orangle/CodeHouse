48826b57f16f116a3b5236f4167ad9d51b58dba0
liuzz <liuzhizhi123@126.com>
2014-07-05 13:18:49 +0800

    [orangleliu]mutil threads

---
+++ python/threading/product_customer.py
@@ -0,0 +1,55 @@
+﻿# -*- coding: utf-8 -*-
+#threading1.py   python2.7.x
+#orangleliu@gmail.com    2014-07-05
+'''
+简单的生产者消费者模型
+see video:https://www.youtube.com/watch?v=i1SW4q9yUEs
+'''
+
+import threading, time, random
+import Queue
+
+
+class Producer :
+    '''生产食物'''
+    def __init__(self):
+        self.food = ['!A','B','C','Apple','Orange','Banana']
+        self.nextTime = 0
+
+    def run(self):
+        global q
+        while (time.clock()<10):
+            if (self.nextTime < time.clock()):
+                f = self.food[random.randrange(len(self.food))]
+                q.put(f)
+                print "Add %s"%f
+                self.nextTime += random.random()
+
+class Customer:
+    '''
+    消费掉食物
+    '''
+    def __init__(self):
+        self.nextTime = 0
+
+    def run(self):
+        global q
+        while(time.clock() < 10):
+            if (self.nextTime < time.clock() and not q.empty()):
+                f = q.get()
+                print "Remove %s"%f
+                self.nextTime += random.random()*2
+
+
+if __name__ ==  "__main__":
+    q = Queue.Queue(10)
+
+    p = Producer()
+    c = Customer()
+
+    pt =  threading.Thread(target=p.run, args=())
+    ct =  threading.Thread(target=c.run, args=())
+    pt.start()
+    ct.start()
+
+
---
+++ python/threading/threading1.py
@@ -0,0 +1,33 @@
+﻿# -*- coding: utf-8 -*-
+#threading1.py   python2.7.x
+#orangleliu@gmail.com    2014-07-05
+'''
+多线程的使用
+threading  is old and outdate
+better choice is threading
+
+see video:https://www.youtube.com/watch?v=i1SW4q9yUEs
+'''
+import threading, random
+
+def Splitter(words):
+    str_list = words.split()
+    new_list = []
+    while (str_list):
+        new_list.append(str_list.pop(random.randrange(0, len(str_list))))
+    print (' '.join(new_list))
+
+if __name__ == '__main__':
+    sentance = "This is new day, do you like shijian ? ok ok ok ok "
+    numOfThreads = 5
+    threadList = []
+
+    for i in range(numOfThreads):
+        t = threading.Thread(target=Splitter, args=(sentance,))
+        t.start()
+        threadList.append(t)
+
+    print ("Thread count is "+str(threading.activeCount()))
+    print ('Exiting ...')
+
+
