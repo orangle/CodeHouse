fb12dae3ba7fd0d94a8323da3dd55c7e5dfdfbbf
liuzz <liuzhizhi123@126.com>
2014-07-19 23:31:20 +0800

    [orangleliu]log test

---
+++ python/log/log_test1.py
@@ -0,0 +1,16 @@
+﻿#-*- coding: utf-8 -*-
+#log_test1.py   python2.7.x
+#author: orangleliu       2014-07-19
+'''
+两个文件中都有log配置
+执行的时候怎么写log文件
+'''
+
+
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s', filename='test1.log')
+logger = logging.getLogger('test1')
+logger.setLevel(logging.DEBUG)
+
+def fun_test1():
+    logger.info("test1-log")
---
+++ python/log/log_test2.py
@@ -0,0 +1,18 @@
+﻿#-*- coding: utf-8 -*-
+#log_test2.py   python2.7.x
+#author: orangleliu       2014-07-19
+
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s', filename='test2.log')
+logger = logging.getLogger('test2')
+logger.setLevel(logging.DEBUG)
+
+from log_test1 import fun_test1
+
+def fun_test2():
+    fun_test1()
+    logger.info("test2-log")
+
+
+if __name__ == "__main__":
+    fun_test2()
