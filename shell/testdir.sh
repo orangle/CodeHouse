157ae9133021764c8a78bbaf70f1b34efb7fc212
liuzz <liuzhizhi123@126.com>
2014-08-04 01:16:19 +0800

    [orangleliu]format

--- shell/testdir.sh
+++ shell/testdir.sh
@@ -14,38 +14,38 @@ Check_Dir()
 {
 if [ -d "$TestDir" ]		
 then 
-		TW=`ls -ld /tmp/lzz/|awk '{print $1}'|sed 's/d//g'|grep 'w'|wc -l`
-		TR=`ls -ld /tmp/lzz/|awk '{print $1}'|sed 's/d//g'|grep 'r'|wc -l`
-		if [ "$TW" -ne 0 -a "$TR" -ne 0 ]
-		then 
-				echo "$TestDir can writted and readed !"
-		else
-				echo "$TestDir can not writted and readed !"
-				echo -n "Do you add write and rend authority [Y|N]"
-				read tt
-				case $tt in 
-						Y|y)
-						chmod 755 $TestDir
-								if [ $? -eq 0 ]
-								then 
-										echo "add write and read authority ok ...."
-								else
-										echo "add write add read authority fail...."
-										return 1
-								fi 
-						;;
-						N|n)
-								return 1
-						;;
-						*)
-								echo "error"
-								return 1
-						;;
-				esac 
-		fi 
+	TW=`ls -ld /tmp/lzz/|awk '{print $1}'|sed 's/d//g'|grep 'w'|wc -l`
+	TR=`ls -ld /tmp/lzz/|awk '{print $1}'|sed 's/d//g'|grep 'r'|wc -l`
+	if [ "$TW" -ne 0 -a "$TR" -ne 0 ]
+	then 
+		echo "$TestDir can writted and readed !"
+	else
+		echo "$TestDir can not writted and readed !"
+		echo -n "Do you add write and rend authority [Y|N]"
+		read tt
+		case $tt in 
+			Y|y)
+			chmod 755 $TestDir
+				if [ $? -eq 0 ]
+				then 
+					echo "add write and read authority ok ...."
+				else
+					echo "add write add read authority fail...."
+					return 1
+				fi 
+			;;
+			N|n)
+				return 1
+			;;
+			*)
+				echo "error"
+				return 1
+			;;
+		esac 
+	fi 
 else
-		echo "not have this dir"
-		return 1
+	echo "not have this dir"
+	return 1
 fi 
 }
 
@@ -55,10 +55,10 @@ fi
 Main()
 {
 Check_Dir
-		if [ $? -eq 1 ]
-		then 
-				exit 1
-		fi
+	if [ $? -eq 1 ]
+	then 
+		exit 1
+	fi
 }
 
 Main
