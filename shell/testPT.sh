30acb0265f1cedf2c26cb0a883e1a1702022e99b
liuzz <liuzhizhi123@126.com>
2014-08-08 19:35:57 +0800

    [orangleliu]输入参数，判断参数的脚本

---
+++ shell/testPT.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#title: testPT.sh
+#atuhor: orangleliu
+#date: 2014-08-08
+#desc:
+
+#=====================
+#Function  Check
+#=====================
+Check()
+{
+if [ $# -ne 2 ]   #charge params num
+then 
+	echo "You must input two params"
+	return 1
+else
+	all="$@" #get all input value
+	secd=`echo $all|awk '{print $2}'`
+	expr 1 + $secd > /dev/null 2>&1
+	if [ $? -eq 0 ]
+	then 
+		echo "$all"
+	else
+		echo "Sorry age is a number" 
+		return 1
+	fi
+fi
+}
+
+#===================
+#Function main()
+#==================
+Main()
+{
+Check $1 $2
+	if [ $? -eq 1 ]
+	then 
+		exit
+	fi
+}
+
+Main $1 $2
