bb2947be8a0cf7f61acb6007d9ef1f5e5909ac56
liuzz <liuzhizhi123@126.com>
2014-12-25 10:19:33 +0800

    zeromq

---
+++ python/zeromq/client1.py
@@ -0,0 +1,14 @@
+﻿#-*- coding: utf-8 -*-
+#python2.7x
+#author: orangleliu@gmail.com 2014-12-19
+#clent1.py
+
+import zmq
+
+adss = "tcp://127.0.0.1:8888"
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect(adss)
+
+socket.send("hello ..")
+print socket.recv()
---
+++ python/zeromq/req.py
@@ -0,0 +1,27 @@
+﻿#-*- coding: utf-8 -*-
+#python2.7x
+#author: orangleliu@gmail.com 2014-12-19
+#req.py
+'''
+client, 一个客户端可以连接多个服务端，多个服务端部署在一个端口上
+'''
+
+import zmq
+import random
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.bind("tcp://*:8888")
+
+time.sleep(1)
+
+for i in range(9):
+    a, b = random.randint(0,100), random.randint(0,100)
+    print 'num:  %s + %s'%(a, b)
+    socket.send_multipart([str(a),str(b)])
+    rep = socket.recv()
+    print ' = %s'%rep
+
+
+
