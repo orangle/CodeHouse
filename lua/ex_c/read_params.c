5ed6a97a108830aab2e9ce5c102fdcc626920442
orangle <orangleliu@gmail.com>
2016-07-14 23:14:44 +0800

    lua c api

---
+++ lua/ex_c/read_params.c
@@ -0,0 +1,34 @@
+#include "lua.h"
+#include "lauxlib.h"
+
+//C API 最主要的就是理解 lua state stack 的工作方式
+int main(){
+    //初始化 state
+    lua_State *L = luaL_newstate();
+    //载入 lib
+    luaL_openlibs(L);
+    //执行lua 文件
+    luaL_dofile(L, "hello.lua");
+
+    //执行函数
+
+    //执行带有参数的函数
+
+    //获取函数的返回值
+
+
+    //读取lua文件中的变量 L是一个栈 
+    lua_getglobal(L, "lzz");
+    lua_getglobal(L, "foo");
+    int i = lua_tonumber(L, -1);
+    printf("foo is %d\n", i);
+
+    if(lua_isnil(L, -2)){
+        printf("lzz is nil\n"); 
+    }else{
+        int j = lua_tonumber(L, -2);
+        printf("lzz is %d\n", j);
+    }
+
+    return 1;
+}
