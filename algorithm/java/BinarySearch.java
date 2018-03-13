d611096dd263d48129924a16343482f6aa18d75b
orangle <orangleliu@gmail.com>
2018-03-13 22:44:29 +0800

    css

---
+++ algorithm/java/BinarySearch.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+public class BinarySearch {
+	public static int rank(int key, int[] a){
+		int lo = 0;
+		int hi = a.length - 1;
+		while( lo <= hi){
+			int mid = lo + (hi - lo)/2;
+			if (key < a[mid]) {
+				hi = mid -1;
+			}else if (key > a[mid]) {
+				lo = mid + 1;
+			}else{
+				return mid;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args){
+		int[] whitelist = In.readInts(args[0]);
+		Arrays.sort(whitelist);
+		while (!StdIn.isEmpty()){
+			int key = StdIn.readInt();
+			if (rank(key, whitelist) < 0){
+				StdOut.println(key);
+			}
+		}
+	}
+}
--- algorithm/java/readme.md
+++ algorithm/java/readme.md
@@ -3,3 +3,8 @@
 
 算法 第四版，Java语言描述为主
 
+> 算法是计算机科学的基础，是这个领域研究的核心
+
+* [课程主页](http://algs4.cs.princeton.edu/home/)
+* [Github 主页](https://github.com/kevin-wayne/algs4)
+* 例子的安装使用 [安装使用参考地址](http://algs4.cs.princeton.edu/linux/)
